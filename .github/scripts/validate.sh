5d4736f362d3e165a828cfd892c7537955347b1d
Rob Cowsill <42620235+rcowsill@users.noreply.github.com>
2024-05-30 12:09:18 +0100

    Update validator (SVG/CSS fix)

--- .github/scripts/validate.sh
+++ .github/scripts/validate.sh
@@ -6,7 +6,7 @@ RUN_OPTS=(
   "--volume=.:/src/JPEGSawmill/"
 )
 
-IMAGE_DIGEST="sha256:14ba09c8f3236fce4c0d46cde03a8471fa63f968f161cb6165bb2a8c9477a5aa"
+IMAGE_DIGEST="sha256:f4ceed83064a348fd23529039ce8634f8ba49ce5df26a5f10f94adf384cbebce"
 VNU_IMAGE="ghcr.io/validator/validator@$IMAGE_DIGEST"
 
 docker run "${RUN_OPTS[@]}" "$VNU_IMAGE" bash - <.github/scripts/vnu.sh
